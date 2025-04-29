919ef3771a686f760f5d6a3d651132183e1d116f
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-04-29 16:31:51 -0300

    Atividade 9
    Faça um programa que receba o ano de nascimento de uma pessoa e o ano atual

---
+++ lista1/ex9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void funcIdade() {
+    time_t ltime;
+    struct tm *newtime;
+    int anoDeNascimento;
+    int idadeAtual;
+    int idadeEm2050;
+
+    // Get the current time
+    time(&ltime);
+
+    // Convert to local time
+    newtime = localtime(&ltime);
+
+    // tm_year is years since 1900, so add 1900 to get the current year
+    int ano = newtime->tm_year + 1900;
+    printf("Qual é seu ano de nascimento: ");
+    scanf("%d", &anoDeNascimento);
+    idadeAtual = ano - anoDeNascimento;
+    idadeEm2050 = 2050 - anoDeNascimento;
+    printf("Ano: %d\n", ano);
+    printf("Idade Atual: %d\n", idadeAtual);
+    printf("Idade em 2050: %d\n", idadeEm2050);
+}
+
+
+int main() {
+    funcIdade();
+    return 0;
+}
