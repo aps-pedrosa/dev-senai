cca6d841b99ee5b146ec4d76b392037181df188a
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-04-29 15:23:43 -0300

    Aumentar Salario em 25%

---
+++ lista1/ex5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void aumentoSalario(void) {
+    float salario;
+
+    printf("Qual é o salário do funcionário em R$: ");
+    scanf("%f", &salario);
+    if (salario <= 0) {
+
+    printf("Erro: O salário não pode ser menor ou igual a zero!");
+        exit(0);
+    };
+    float novosalario = salario * 1.25;
+    printf("O novo salário é de: R$ %.2f", novosalario);
+
+}
+
+int main(void) {
+    aumentoSalario();
+}
