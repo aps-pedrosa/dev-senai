9c227bed8f7f6d860080efe1fc881bca485d5b2c
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-05-01 20:48:58 -0300

    ex18
    Entrar com um número e imprimir a seguinte saída:
    a. Numero:
    b. Quadrado:
    c. Raiz quadrada:

---
+++ lista1/ex18.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h> 
+
+void exibirInformacoesNumero() {
+    float numero, quadrado, raiz;
+
+    printf("Digite um número: ");
+    scanf("%f", &numero);
+
+    quadrado = numero * numero;
+    raiz = sqrt(numero);
+
+    printf("\na. Número: %.2f\n", numero);
+    printf("b. Quadrado: %.2f\n", quadrado);
+    printf("c. Raiz quadrada: %.2f\n", raiz);
+}
+
+int main() {
+    exibirInformacoesNumero();
+    return 0;
+}
