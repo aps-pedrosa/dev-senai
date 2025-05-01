0327d0079eaba6ebfa064a0710ee116c78871c02
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-05-01 20:45:15 -0300

    ex14
    Escrever um algoritmo que leia o nome e as três notas obtidas por um aluno durante o
    semestre. Calcular a sua média (aritmética), informar o nome e sua menção aprovado
    (media &gt;= 7), Reprovado (media &lt;7).

---
+++ lista1/ex14.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Função que avalia o aluno e imprime os resultados
+void avaliarAluno() {
+    char nome[100];
+    float nota1, nota2, nota3, media;
+    char *status;
+
+    printf("Forneça o nome do aluno: ");
+    fgets(nome, sizeof(nome), stdin);
+
+    printf("Forneça a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Forneça a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Forneça a terceira nota: ");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    if (media >= 7) {
+        status = "Aprovado";
+    } else {
+        status = "Reprovado";
+    }
+
+
+
+    printf("\nNome do Aluno: %s", nome);
+    printf("Média: %.2f\n", media);
+    printf("Menção: %s\n", status);
+}
+
+int main() {
+    avaliarAluno();  // Chamada da função
+    return 0;
+}
