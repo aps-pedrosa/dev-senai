ebb83524b5706c7f76a8ef08748307093913d04d
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-05-01 20:47:29 -0300

    ex16
    Ler um número real e imprimir a terça parte deste número.

---
+++ lista1/ex16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void imprimirTercaParte() {
+    float numero, tercaParte;
+
+    printf("Digite um número real: ");
+    scanf("%f", &numero);
+
+    tercaParte = numero / 3.0;
+
+    printf("A terça parte de %.2f é %.2f\n", numero, tercaParte);
+}
+
+int main() {
+    imprimirTercaParte();
+    return 0;
+}
