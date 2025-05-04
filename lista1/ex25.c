874c6795dfe21c2c6676f7f8b00af1e50696c311
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-05-04 00:34:02 -0300

    Atividades Sabado

---
+++ lista1/ex25.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void funcSomaComCondicao() {
+    int num1, num2, soma, resultado;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    if (soma > 20) {
+        resultado = soma + 8;
+    } else {
+        resultado = soma - 5;
+    }
+
+    printf("Resultado: %d\n", resultado);
+}
+
+// int main() {
+//     funcSomaComCondicao();
+//     return 0;
+// }
