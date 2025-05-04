61436e267ec33fe65644b207f2597af7664c8b42
Vurse <136754212+VurseDev@users.noreply.github.com>
2025-05-04 00:28:57 -0300

    Atividades Sabado

---
+++ lista1/ex21.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+
+void funcNumeroMaiorQue20() {
+    int numeroMaior;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numeroMaior);
+
+    if (numeroMaior >= 20) {
+        printf("Metade do Numero: %d", numeroMaior / 2);
+    } else if (numeroMaior < 20) {
+        printf("Seu numero não é maior ou igual a 20!");
+    }
+}
+
+//
+// int main() {
+//     funcNumeroMaiorQue20();
+// }
